849ec2c4991b6e2a5a5b14fe6cd58fc5a607b597
alonecoder122 <ak7616308@gmail.com>
2023-07-21 01:21:15 +0530

    Cart.js updated

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from "react";
-import { useSelector, useDispatch, Navigate } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   deleteItemFromCartAsync,
   selectItems,
@@ -8,7 +8,7 @@ import {
 import { Link } from "react-router-dom";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-
+import { Navigate } from "react-router-dom";
 export default function Cart() {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(true);
@@ -30,9 +30,7 @@ export default function Cart() {
   };
   return (
     <>
-      {/* {!items.length && cartLoaded && (
-        <Navigate to="/" replace={true}></Navigate>
-      )} */}
+      {!items.length && <Navigate to="/" replace={true}></Navigate>}
       <div>
         <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
@@ -99,6 +97,7 @@ export default function Cart() {
               </ul>
             </div>
           </div>
+
           <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
             <div className="flex justify-between my-2 text-base font-medium text-gray-900">
               <p>Subtotal</p>
