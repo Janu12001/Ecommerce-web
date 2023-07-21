ecd37a40874ccdc496915305c8ee885a31df66fb
alonecoder122 <ak7616308@gmail.com>
2023-07-21 22:05:15 +0530

    updated authpage

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -11,10 +11,23 @@ export function createUser(userData) {
   });
 }
 
+//update User
+export function updateUser(update) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/users/" + update.id, {
+      method: "PATCH",
+      body: JSON.stringify(update),
+      headers: { "content-type": "application/json" },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
 export function checkUser(loginInfo) {
   return new Promise(async (resolve, reject) => {
     const email = loginInfo.email;
-    const password = loginInfo.password;
+    const password = loginInfo.pasword;
     const response = await fetch("http://localhost:8080/users?email=" + email);
     const data = await response.json();
     console.log({ data });
