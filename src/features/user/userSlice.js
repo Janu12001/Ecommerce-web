c2bee2f29092a57b9d1731806930a173d23b2257
alonecoder122 <ak7616308@gmail.com>
2023-07-24 01:51:22 +0530

    user slice file updated

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,15 +1,32 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchLoggedInUserOrders } from "./userAPI";
-
+import { fetchLoggedInUserOrders, fetchLoggedInUser } from "./userAPI";
+import { updateUser } from "./userAPI";
 const initialState = {
   userOrders: [],
   status: "idle",
+  userInfo: null,
 };
 
 export const fetchLoggedInUserOrdersAsync = createAsyncThunk(
   "user/fetchLoggedInUserOrders",
   async (id) => {
     const response = await fetchLoggedInUserOrders(id);
+    return response.data;
+  }
+);
+
+export const fetchLoggedInUserAsync = createAsyncThunk(
+  "user/fetchLoggedInUser",
+  async (id) => {
+    const response = await fetchLoggedInUser(id);
+    return response.data;
+  }
+);
+
+export const updateUserAsync = createAsyncThunk(
+  "user/UpdateUser",
+  async (id) => {
+    const response = await updateUser(id);
     return response.data;
   }
 );
@@ -33,11 +50,30 @@ export const userSlice = createSlice({
         state.status = "idle";
         // this info can be different from logged in user info
         state.userOrders = action.payload;
+      })
+
+      .addCase(updateUserAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updateUserAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        // this info can be different from logged in user info
+        state.userOrders = action.payload;
+      })
+
+      .addCase(fetchLoggedInUserAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchLoggedInUserAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        // this info can be different from logged in user info
+        state.userInfo = action.payload;
       });
   },
 });
 
 export const selectUserOrders = (state) => state.user.userOrders;
+export const selectUserInfo = (state) => state.user.userInfo;
 export const { increment, decrement, incrementByAmount } = userSlice.actions;
 
 export default userSlice.reducer;
