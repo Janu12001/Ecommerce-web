95a76a9b86ce63f5a7c92b7572d2742dd368dc49
alonecoder122 <ak7616308@gmail.com>
2023-07-26 19:35:00 +0530

    admin page set up

---
+++ src/features/admin/ProductListAPI.js
@@ -0,0 +1,73 @@
+// A mock function to mimic making an async request for data
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server url heree
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsById(id) {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server url heree
+    const response = await fetch("http://localhost:8080/products/" + id);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort, pagination) {
+  //filter {"category": "smartphone"}
+  //sort {_sort:"price", order="desc"}
+  //pagination ={_page:1 , _limit=10}//_page=1&_limit=10
+  //Todo: onserver we will support multi values in filter
+
+  //filtering
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+
+  //sorting
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+
+  //pagination
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server url heree
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data: { products: data, totalItems: +totalItems } });
+  });
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server url heree
+    const response = await fetch("http://localhost:8080/categories");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server url heree
+    const response = await fetch("http://localhost:8080/brands");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
