aef1bab4d0958385afe6b1a74c7f4be44c39e992
alonecoder122 <ak7616308@gmail.com>
2023-07-26 19:28:49 +0530

    app file update

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ import UserProfile from "./features/user/components/UserProfile";
 import UserProfilePage from "./pages/UserProfilePage";
 import { fetchLoggedInUser } from "./features/user/userAPI";
 import { fetchLoggedInUserAsync } from "./features/user/userSlice";
+import Logout from "./features/auth/components/Logout";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -86,7 +89,15 @@ const router = createBrowserRouter([
     path: "/profile",
     element: <UserProfilePage></UserProfilePage>,
   },
+  {
+    path: "/logout",
+    element: <Logout></Logout>,
+  },
 
+  {
+    path: "/forgot-password",
+    element: <ForgotPasswordPage></ForgotPasswordPage>,
+  },
   {
     path: "*",
     element: <PageNotFound></PageNotFound>,
