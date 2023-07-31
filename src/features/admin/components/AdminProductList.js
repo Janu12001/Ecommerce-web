50a498faa8fcc201ab0ecf130df652b990923646
alonecoder122 <ak7616308@gmail.com>
2023-08-01 02:29:38 +0530

    AdminProductList file completed

--- src/features/admin/components/AdminProductList.js
+++ src/features/admin/components/AdminProductList.js
@@ -26,7 +26,7 @@ import {
   PlusIcon,
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
-import { ITEM_PER_PAGE } from "../../../app/constants";
+import { ITEM_PER_PAGE, discountedPrice } from "../../../app/constants";
 const sortOptions = [
   { name: "Best Rating", sort: "rating", order: "desc", current: false },
   { name: "Price: Low to High", sort: "price", order: "asc", current: false },
@@ -1095,10 +1095,7 @@ function ProductGrid({ products }) {
                     </div>
                     <div>
                       <p className="text-sm font-medium text-gray-900">
-                        $
-                        {Math.round(
-                          product.price * (1 - product.discountPercentage / 100)
-                        )}
+                        ${discountedPrice(product)}
                       </p>
                       <p className="text-sm  font-medium line-through text-gray-400">
                         ${product.price}
