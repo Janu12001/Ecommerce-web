9613af2b26c7ea3016a99faca72d6080a5aaaec2
alonecoder122 <ak7616308@gmail.com>
2023-08-26 21:43:34 +0530

    added

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -62,7 +62,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-sand.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://frail-sunbonnet-dove.cyclic.cloud/order-success/${currentOrder.id}`,
       },
     });
 
